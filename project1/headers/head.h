fd8cb932904fb120f06a36ba6f001ed24a07a5e0
anton <anton2920@gmail.com>
2018-12-08 12:29:52 +0300

    Anton: base program

---
+++ project1/headers/head.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#define SIZE 1024
+
+void read_key(int *, int *);
+void read_rand(int *, int *);
+void read_file(int *, int *);
+void read_bfile(int *, int *);
+void write_scr(int *, int *);
+void write_file(int *, int *);
+void write_bfile(int *, int *);
+int menu(void);
+int menu2(void);
+int menu3(void);
+int num_elem(void);
+void task(int *, int *);
+
+#endif
