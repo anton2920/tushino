e3f913e2dd2c7f98ef1ede6bdabee4fbc664216e
anton <anton2920@gmail.com>
2019-05-01 18:16:55 +0300

    Anton: refoldering

---
+++ project4/2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <malloc.h>
+
+struct Max{
+	int numb;
+	int count=0;
+};
+
+int main() {
+
+	int n;
+
+	std::cout<<"Enter N: ";
+	std::cin>>n;
+	int *A = (int*)malloc(n * sizeof(int));
+	for(int i=0;i<n;i++){
+		std::cin>>A[i];
+	}
+	Max max;
+
+	for (int i=0;i<n;i++){
+		int count=0;
+
+ 		for (int j=0; j<n; j++) {
+ 			if (A[i]==A[j]) count++;
+ 		}
+ 		if (count>max.count||(count==max.count && A[i]<max.numb)){
+ 			max.count=count;
+ 			max.numb=A[i];
+ 		}
+	}
+
+	std::cout<<"Result array: ";
+
+	for (int i=0;i<n;i++){
+		if (A[i]==max.numb) continue;
+		std::cout<<A[i]<<' ';
+	}
+
+	for (int i=0;i<max.count;i++)
+		std::cout<<max.numb<<' ';
+		
+	free(A);
+	return 0;
+}
---
+++ project4/6.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <string>
+#include <vector>	 
+#include <fstream>   
+#include <algorithm> // for std::sort
+struct Word
+{
+	std::string word;
+	uint16_t order;
+
+	Word(std::string& p_word, uint8_t p_order)
+		: word(std::move(p_word)), order(p_order) {}
+};
+
+bool cmp(Word& a, Word& b)
+{
+	if (a.order == b.order)
+		return a.word < b.word;
+
+	return a.order < b.order;
+}
+
+void classification(std::vector<Word> & data)
+{
+	bool all_caps;
+	std::size_t word_length;
+	for (auto& e : data)
+	{
+		all_caps = true;
+		word_length = e.word.length();
+		for (std::size_t i = 0; i < word_length; ++i)
+		{
+			if (e.word[i] < 'A' || e.word[i] > 'Z')
+			{
+				all_caps = false;
+				break;
+			}
+		}
+		if (all_caps)
+			e.order = 1;
+		else if (word_length >= 3)
+		{
+			if (e.word[word_length - 1] == 'e' &&
+				e.word[word_length - 2] == 'x' &&
+				e.word[word_length - 3] == 'e')
+				e.order = 2;
+
+			else if (e.word[word_length - 1] == 'm' &&
+				e.word[word_length - 2] == 'o' &&
+				e.word[word_length - 3] == 'c')
+				e.order = 3;
+
+			else if (e.word[word_length - 1] == 't' &&
+				e.word[word_length - 2] == 'a' &&
+				e.word[word_length - 3] == 'b')
+				e.order = 4;
+
+			else if (e.word[word_length - 1] == 'c' &&
+				e.word[word_length - 2] == 'o' &&
+				e.word[word_length - 3] == 'd')
+				e.order = 5;
+
+			else if (e.word[word_length - 1] == 't' &&
+				e.word[word_length - 2] == 'x' &&
+				e.word[word_length - 3] == 't')
+				e.order = 6;
+
+			else
+				e.order = 7;
+		}
+		else
+			e.order = 7;
+	}
+}
+
+int main()
+{
+	std::ifstream inFile;
+	inFile.open("6.txt");
+
+	std::vector<Word> data;
+
+	if (inFile.is_open())
+	{
+		std::cout << "Reading data: \n";
+		std::string word;
+		while (inFile >> word)
+		{
+			std::cout << word << '\n';
+			data.emplace_back(word, 0);
+		}
+		
+		classification(data);
+		std::sort(data.begin(), data.end(), cmp);
+
+		std::cout << "\n\n Sorting words:\n";
+		for (auto& e : data)
+			std::cout << e.word << '\n';
+	}
+
+	else
+	{
+		std::cout << "Couldn't open file!\n";
+	}
+	return 0;
+}
